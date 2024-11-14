797be3a35a2febed4c091f102fe6976bc90539f5
root <root@MSI>
2024-11-14 13:29:48 -0800

    added frame rate

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 
 def main():
     pg.init()
+    clock = pg.time.Clock()
+    dt=0
     screen= pg.display.set_mode((SCREEN_WIDTH , SCREEN_HEIGHT))
     while True:
         for event in pg.event.get():
@@ -10,6 +12,9 @@ def main():
                return
         screen.fill((0,0,0))
         pg.display.flip()
+
+        #frame rate
+        det = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
