ff0683c702eae38a9796297178ae77e045644e69
root <root@MSI>
2024-11-14 13:59:02 -0800

    created player

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import pygame as pg
 from constants import *
+from player import Player
 
 def main():
     pg.init()
     clock = pg.time.Clock()
     dt=0
     screen= pg.display.set_mode((SCREEN_WIDTH , SCREEN_HEIGHT))
+    player= Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                return
         screen.fill((0,0,0))
+        player.draw(screen)
         pg.display.flip()
 
         #frame rate
