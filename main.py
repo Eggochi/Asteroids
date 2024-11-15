072b19aeaa64fe1f15ad49e36020487d16c13b81
root <root@MSI>
2024-11-14 18:25:05 -0800

    rotation added

--- main.py
+++ main.py
@@ -13,11 +13,12 @@ def main():
             if event.type == pg.QUIT:
                return
         screen.fill((0,0,0))
+        player.update(dt)
         player.draw(screen)
         pg.display.flip()
 
         #frame rate
-        det = clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
