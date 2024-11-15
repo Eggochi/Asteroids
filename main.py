c0ba34ec17deef2dc8f0ee029ed06e6e8c26103c
root <root@MSI>
2024-11-14 19:11:09 -0800

    created groups

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
-import pygame as pg
+import pygame 
 from constants import *
 from player import Player
 
 def main():
-    pg.init()
-    clock = pg.time.Clock()
-    dt=0
-    screen= pg.display.set_mode((SCREEN_WIDTH , SCREEN_HEIGHT))
+    pygame.init()
+    clock = pygame.time.Clock()
+    screen= pygame.display.set_mode((SCREEN_WIDTH , SCREEN_HEIGHT))
+
+    #groups
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+
+    #grouping
+    Player.containers = (updatables,drawables)
+
     player= Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    dt=0
+    
     while True:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                return
         screen.fill((0,0,0))
-        player.update(dt)
-        player.draw(screen)
-        pg.display.flip()
+        for updatable in updatables:
+            updatable.update(dt)
+        for drawable in drawables:
+            drawable.draw(screen)
+        pygame.display.flip()
 
         #frame rate
         dt = clock.tick(60) / 1000
